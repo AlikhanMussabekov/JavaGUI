5ce53f67dd011e5184fa7cbd34ddd55f117ae6e2
Alikhan <alik10436@gmail.com>
2018-05-05 23:17:34 +0300

    Button Swing

---
+++ Swing/src/ButtonDemo.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ButtonDemo implements ActionListener{
+    JLabel label;
+
+    ButtonDemo(){
+        JFrame jframe = new JFrame("Button example");
+        jframe.setLayout(new FlowLayout());
+        jframe.setSize(275, 100);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton buttonUp = new JButton("Up");
+        JButton buttonDown = new JButton("Down");
+
+        buttonUp.addActionListener(this);
+        buttonDown.addActionListener(this);
+
+        jframe.add(buttonUp);
+        jframe.add(buttonDown);
+
+        label = new JLabel("Press button");
+
+        jframe.add(label);
+
+        jframe.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand().equals("Up")){
+            label.setText("You pressed Up.");
+        }else {
+            label.setText("You pressed Down.");
+        }
+    }
+}
--- Swing/src/SwingDemo.java
+++ Swing/src/SwingDemo.java
@@ -1,26 +1,14 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class SwingDemo {
     public static void main(String[] args){
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new SwingDemo();
+                new ButtonDemo();
             }
         });
     }
 
-    SwingDemo(){
-        JFrame jframe = new JFrame("First app");
-
-        jframe.setSize(275, 100);
-
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JLabel jlabel = new JLabel("Hello World!");
-
-        jframe.add(jlabel);
-
-        jframe.setVisible(true);
-    }
 }
