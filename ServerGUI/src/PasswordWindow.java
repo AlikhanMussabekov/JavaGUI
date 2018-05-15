9ff5bfac846d6fad5fcd58ea4eb44bdd2b1419df
Alikhan <alik10436@gmail.com>
2018-05-15 21:49:58 +0300

    Password Window alignment added

--- ServerGUI/src/PasswordWindow.java
+++ ServerGUI/src/PasswordWindow.java
@@ -3,42 +3,50 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-class PasswordWindow implements ActionListener {
+class PasswordWindow extends JFrame implements ActionListener {
 
     JTextField passwordTextField;
     JButton enterButton;
     JLabel greetingLabel,passwordCheckLabel;
 
+    JFrame passwordWindowfFrame = new JFrame("Вход");
+
     PasswordWindow(){
-        JFrame passwordWindowfFrame = new JFrame("Entrance");
-
-
-        passwordWindowfFrame.setLayout(new FlowLayout());
 
         passwordWindowfFrame.setSize(300,150);
-
         passwordWindowfFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        passwordWindowfFrame.setMinimumSize(new Dimension(300,150));
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
         passwordTextField = new JPasswordField(10);
         passwordTextField.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-
+        passwordTextField.setMaximumSize(new Dimension(150,20));
 
         passwordTextField.setActionCommand("textFieldAction");
 
-        enterButton = new JButton("Enter");
+        enterButton = new JButton("Войти");
 
         passwordTextField.addActionListener(this);
         enterButton.addActionListener(this);
 
         greetingLabel = new JLabel("Добро пожаловать!");
+        greetingLabel.setFont(new Font("Courier New", Font.ITALIC, 25));
+
         passwordCheckLabel = new JLabel();
 
-        greetingLabel.setAlignmentY(Component.CENTER_ALIGNMENT);
+        greetingLabel.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+        passwordTextField.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+        passwordCheckLabel.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+        enterButton.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 
-        passwordWindowfFrame.add(greetingLabel);
-        passwordWindowfFrame.add(passwordTextField);
-        passwordWindowfFrame.add(passwordCheckLabel);
-        passwordWindowfFrame.add(enterButton);
+        panel.add(greetingLabel);
+        panel.add(passwordTextField);
+        panel.add(passwordCheckLabel);
+        panel.add(enterButton);
+
+        passwordWindowfFrame.add(panel);
 
         passwordWindowfFrame.setVisible(true);
 
@@ -46,12 +54,14 @@ class PasswordWindow implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        if(ae.getActionCommand().equals("Enter")){
+        if(ae.getActionCommand().equals("Войти")){
 
             String enteredPassword = passwordTextField.getText();
             if(enteredPassword.equals("newpass")){
                 passwordCheckLabel.setText("Верный пароль");
+                passwordCheckLabel.setForeground(Color.GREEN);
                 passwordTextField.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+                passwordWindowfFrame.dispose();
             }else{
                 passwordTextField.setText("");
                 passwordCheckLabel.setText("Неверный пароль");
@@ -59,7 +69,8 @@ class PasswordWindow implements ActionListener {
                 passwordTextField.setBorder(BorderFactory.createLineBorder(Color.RED));
             }
         }else {
-            passwordCheckLabel.setText("Введите пароль");
+            //passwordCheckLabel.setText("Введите пароль");
+            //passwordCheckLabel.;
         }
     }
 }
